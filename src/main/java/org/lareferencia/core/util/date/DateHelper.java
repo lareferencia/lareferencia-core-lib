b02b2ce83f33342ca70a93bf82c5ac41efac743f
João Bosco Jares Alves Chaves <jbjares@gmail.com>
2023-05-12 12:30:45 -0300

    - Improvements on Exception handler.

--- src/main/java/org/lareferencia/core/util/date/DateHelper.java
+++ src/main/java/org/lareferencia/core/util/date/DateHelper.java
@@ -21,6 +21,7 @@
 
 package org.lareferencia.core.util.date;
 
+import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
@@ -32,174 +33,188 @@ import lombok.Setter;
 
 public class DateHelper {
 
-    @Setter
-    private Set<IDateTimeFormatter> dateTimeFormatters;
+	@Setter
+	private Set<IDateTimeFormatter> dateTimeFormatters;
 
-    static DateTimeFormatter HUMAN_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-	static DateTimeFormatter MACHINE_DATE_TIME_FORMATTER =  DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"); // Quoted "Z" to indicate UTC, no timezone offset
+	static DateTimeFormatter HUMAN_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	static DateTimeFormatter MACHINE_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"); // Quoted
+																													// "Z"
+																													// to
+																													// indicate
+																													// UTC,
+																													// no
+																													// timezone
+																													// offset
 
+	/**
+	 * Parses a string date and return a LocalDateTime can throw an exception if
+	 * string date doesn't match any pre-configured dateTimeFormatters
+	 * 
+	 * @param strDate
+	 * @return
+	 */
+	public LocalDateTime parseDate(String strDate) {
+		if (strDate == null) {
+			return null;
+		}
+		Optional<DateTimeFormatter> optFormatter = getDateFormatterFromString(strDate);
+		if (optFormatter.isPresent()) {
+			return LocalDateTime.parse(strDate, optFormatter.get());
+		}
+		try {
+			return LocalDateTime.parse(strDate);
+		} catch (Exception e) {
+		}
 
-    /**
-     * Parses a string date and return a LocalDateTime can throw an exception if
-     * string date doesn't match any pre-configured dateTimeFormatters
-     * 
-     * @param strDate
-     * @return
-     */
-    public LocalDateTime parseDate(String strDate) {
-        if (strDate == null) {
-            return null;
-        }
-        Optional<DateTimeFormatter> optFormatter = getDateFormatterFromString(strDate);
-        if (optFormatter.isPresent()) {
-            return LocalDateTime.parse(strDate, optFormatter.get());
-        }
-        try {
-            return LocalDateTime.parse(strDate);
-        } catch ( Exception e ) {}
-        
-        
-        throw new DateTimeParseException("Date in an unsupported date format: " + strDate, strDate, 0);
-    }
+		throw new DateTimeParseException("Date in an unsupported date format: " + strDate, strDate, 0);
+	}
 
-    /**
-     * Get Instant string
-     * 
-     * @param date
-     * @return
-     */
-    public static String getInstantDateString(LocalDateTime date) {
-        return DateTimeFormatter.ISO_INSTANT.format(date.toInstant(ZoneOffset.UTC));
-    }
+	/**
+	 * Get Instant string
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static String getInstantDateString(LocalDateTime date) {
+		return DateTimeFormatter.ISO_INSTANT.format(date.toInstant(ZoneOffset.UTC));
+	}
 
-    /**
-     * From a date, returns a preformatted String with the<br/>
-     * pattern: "yyyy-MM-dd'T'HH:mm:ss'Z'"
-     * 
-     * @param date
-     * @return
-     */
-    public static String getDateTimeMachineString(LocalDateTime date) {
-        return MACHINE_DATE_TIME_FORMATTER.format(date);
-    }
+	/**
+	 * From a date, returns a preformatted String with the<br/>
+	 * pattern: "yyyy-MM-dd'T'HH:mm:ss'Z'"
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static String getDateTimeMachineString(LocalDateTime date) {
+		return MACHINE_DATE_TIME_FORMATTER.format(date);
+	}
 
+	/**
+	 * From a date, returns a preformatted String with the<br/>
+	 * pattern: "yyyy-MM-dd HH:mm:ss"
+	 *
+	 * @param date
+	 * @param pattern
+	 *
+	 * @return
+	 */
+	public static String getDateTimeFormattedString(LocalDateTime date, String pattern) {
+		return DateTimeFormatter.ofPattern(pattern).format(date);
+	}
 
-    /**
-     * From a date, returns a preformatted String with the<br/>
-     * pattern: "yyyy-MM-dd HH:mm:ss"
-     *
-     * @param date
-     * @param pattern
-     *
-     * @return
-     */
-    public static String getDateTimeFormattedString(LocalDateTime date, String pattern) {
-        return DateTimeFormatter.ofPattern(pattern).format(date);
-    }
+	/**
+	 * From a date, returns a preformatted String with the<br/>
+	 * pattern derived from granularity "yyyy-MM-ddTHH:mm:ssZ"
+	 *
+	 * @param date
+	 * @param granularity
+	 *
+	 * @return
+	 */
+	public static String getDateTimeFormattedStringFromGranularity(LocalDateTime date, String granularity) {
+		return getDateTimeFormattedString(date, getPatternFromGranularity(granularity));
+	}
 
-    /**
-     * From a date, returns a preformatted String with the<br/>
-     * pattern derived from granularity "yyyy-MM-ddTHH:mm:ssZ"
-     *
-     * @param date
-     * @param granularity
-     *
-     * @return
-     */
-    public static String getDateTimeFormattedStringFromGranularity(LocalDateTime date, String granularity) {
-        return getDateTimeFormattedString(date, getPatternFromGranularity(granularity));
-    }
+	private static String getPatternFromGranularity(String granularity) {
+		String pattern = pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 
-    private static String getPatternFromGranularity(String granularity) {
-        String pattern = pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+		if (granularity != null) {
 
-        if (granularity != null) {
+			switch (granularity) {
 
-            switch (granularity) {
+			case "yyyy-MM-dd":
+				return "yyyy-MM-dd";
 
-            case "yyyy-MM-dd":
-                return "yyyy-MM-dd";
+			case "YYYY-MM-DD":
+				return "yyyy-MM-dd";
 
-            case "YYYY-MM-DD":
-                return "yyyy-MM-dd";
+			case "yyyy-MM-ddTHH":
+				return "yyyy-MM-dd'T'HH";
 
-            case "yyyy-MM-ddTHH":
-                return  "yyyy-MM-dd'T'HH";
+			case "yyyy-MM-ddTHH:mm":
+				return "yyyy-MM-dd'T'HH:mm";
 
-            case "yyyy-MM-ddTHH:mm":
-                return  "yyyy-MM-dd'T'HH:mm";
+			case "yyyy-MM-ddTHH:mm:ss":
+				return "yyyy-MM-dd'T'HH:mm:ss";
 
-            case "yyyy-MM-ddTHH:mm:ss":
-                return  "yyyy-MM-dd'T'HH:mm:ss";
+			case "yyyy-MM-ddTHH:mm:ssZ":
+				return "yyyy-MM-dd'T'HH:mm:ss'Z'";
 
-            case "yyyy-MM-ddTHH:mm:ssZ":
-                return  "yyyy-MM-dd'T'HH:mm:ss'Z'";
+			case "yyyy-MM-ddTHH:mm:ss.SSSZ":
+				return "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
-            case "yyyy-MM-ddTHH:mm:ss.SSSZ":
-                return  "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+			case "yyyy-MM-ddTHH:mm:ss.SSS":
+				return "yyyy-MM-dd'T'HH:mm:ss.SSS";
 
-            case "yyyy-MM-ddTHH:mm:ss.SSS":
-                return  "yyyy-MM-dd'T'HH:mm:ss.SSS";
+			case "yyyy-MM-ddTHH:mm:ss.SSSXXX":
+				return "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
 
-            case "yyyy-MM-ddTHH:mm:ss.SSSXXX":
-                return  "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+			case "yyyy-MM-ddTHH:mm:ss.SSSXXXZ":
+				return "yyyy-MM-dd'T'HH:mm:ss.SSSXXX'Z'";
 
-            case "yyyy-MM-ddTHH:mm:ss.SSSXXXZ":
-                return  "yyyy-MM-dd'T'HH:mm:ss.SSSXXX'Z'";
+			}
+		}
+		return pattern;
+	}
 
-            }
-        }
-        return pattern;
-    }
+	/**
+	 * From a date, returns a preformatted String with the<br/>
+	 * pattern: "yyyy-MM-dd HH:mm:ss"
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static String getDateTimeHumanString(LocalDateTime date) {
+		return HUMAN_DATE_TIME_FORMATTER.format(date);
+	}
 
-    /**
-     * From a date, returns a preformatted String with the<br/>
-     * pattern: "yyyy-MM-dd HH:mm:ss"
-     * 
-     * @param date
-     * @return
-     */
-    public static String getDateTimeHumanString(LocalDateTime date) {
-        return HUMAN_DATE_TIME_FORMATTER.format(date);
-    }
+	/**
+	 * Validates if a string date is a valid date according to configured
+	 * DateTimeFormatters
+	 * 
+	 * @param dateStr
+	 * @return
+	 */
+	public boolean isValid(String dateStr) {
 
-    /**
-     * Validates if a string date is a valid date according to configured
-     * DateTimeFormatters
-     * 
-     * @param dateStr
-     * @return
-     */
-    public boolean isValid(String dateStr) {
+		for (IDateTimeFormatter dateTimeFormatter : dateTimeFormatters) {
+			IDateValidator dateValidator = new DateValidator(dateTimeFormatter.getFormatter());
 
-        for (IDateTimeFormatter dateTimeFormatter : dateTimeFormatters) {
-            IDateValidator dateValidator = new DateValidator(dateTimeFormatter.getFormatter());
+			// if there is at least one valid date
+			if (dateValidator.isValid(dateStr)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-            // if there is at least one valid date
-            if (dateValidator.isValid(dateStr)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	/**
+	 * Get the proper DateTimeFromatter that verifies that a string is a date
+	 * 
+	 * @param dateStr
+	 * @return
+	 */
+	public Optional<DateTimeFormatter> getDateFormatterFromString(String dateStr) {
 
-    /**
-     * Get the proper DateTimeFromatter that verifies that a string is a date
-     * 
-     * @param dateStr
-     * @return
-     */
-    public Optional<DateTimeFormatter> getDateFormatterFromString(String dateStr) {
+		for (IDateTimeFormatter dateTimeFormatter : dateTimeFormatters) {
+			IDateValidator dateValidator = new DateValidator(dateTimeFormatter.getFormatter());
 
-        for (IDateTimeFormatter dateTimeFormatter : dateTimeFormatters) {
-            IDateValidator dateValidator = new DateValidator(dateTimeFormatter.getFormatter());
+			// if there is at least one valid date
+			if (dateValidator.isValid(dateStr)) {
+				return Optional.of(dateTimeFormatter.getFormatter());
+			}
+		}
+		return Optional.ofNullable(null);
+	}
 
-            // if there is at least one valid date
-            if (dateValidator.isValid(dateStr)) {
-                return Optional.of(dateTimeFormatter.getFormatter());
-            }
-        }
-        return Optional.ofNullable(null);
-    }
-
+	
+	public boolean isValidLocalDateTime(LocalDateTime dateTime) {
+		try {
+			dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+			return true;
+		} catch (DateTimeException e) {
+			return false;
+		}
+	}
 }
