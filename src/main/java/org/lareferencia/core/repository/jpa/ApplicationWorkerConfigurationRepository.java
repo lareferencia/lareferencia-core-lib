99e0eb5f8dfa2da1fd0062358a9558eac136eaf9
Lautaro Matas <lmatas@gmail.com>
2026-08-30 20:23:00 +0200

    Hide worker configuration repository from Data REST

--- src/main/java/org/lareferencia/core/repository/jpa/ApplicationWorkerConfigurationRepository.java
+++ src/main/java/org/lareferencia/core/repository/jpa/ApplicationWorkerConfigurationRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import org.lareferencia.core.domain.ApplicationWorkerConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface ApplicationWorkerConfigurationRepository extends JpaRepository<ApplicationWorkerConfiguration, Long> {
     List<ApplicationWorkerConfiguration> findAllByEngineTypeOrderByWorkerKeyAsc(String engineType);
     Optional<ApplicationWorkerConfiguration> findByEngineTypeAndWorkerKey(String engineType, String workerKey);
